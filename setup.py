3045d32769e83828f8a43586c0222f7d7f753869
David-Lor <17401854+david-lor@users.noreply.github.com>
2019-07-05 22:19:12 +0200

    Update Setup (v0.0.3, test compatibility)

--- setup.py
+++ setup.py
@@ -7,22 +7,25 @@ with open("README.md") as f:
 
 setup(
     name="dotenv-settings-handler",
-    version="0.0.2",
+    version="0.0.3",
     description="Settings handler to load settings from a DotEnv file or system env variables, "
                 "using python-dotenv and pydantic.",
     author="David Lorenzo",
     url="https://github.com/David-Lor/Python-DotEnv-Settings-Handler",
     packages=("dotenv_settings_handler",),
     install_requires=("pydantic",),
+    setup_requires=("pytest-runner",),
+    tests_require=("pytest",),
+    test_suite="pytest",
     license="Apache 2.0",
-    classifiers=(
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
-    ),
+    ],
     long_description_content_type="text/markdown",
     long_description=long_description,
 )
